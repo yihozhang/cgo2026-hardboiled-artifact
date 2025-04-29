ecbbe5b408d7ab80213bdbc29aad9a63f5465e70
Yihong Zhang <yz489@cs.washington.edu>
2025-04-28 23:47:32 -0700

    checkpoint

--- instrsel-benchmarks/einsum_flat_1x1.cpp
+++ instrsel-benchmarks/einsum_flat_1x1.cpp
@@ -13,53 +13,109 @@ bool matmul_bf16(Halide::Target target) {
     const int acc = 4096;
 
     Var x("x"), y("y"), z("z");
-    ImageParam A(BFloat(16), 2, "A");
-    ImageParam B(BFloat(16), 2, "B");
-    ImageParam C(BFloat(16), 2, "C");
+    ImageParam A(Float(16), 2, "A");
+    ImageParam B(Float(16), 2, "B");
+    ImageParam C(Float(16), 2, "C");
 
     RDom r(0, acc, 0, acc, "acc");
 
     Func mm("matmul");
-    mm(x, y) = cast<float>(0);
-    // mm(x, w) = A(x, y) * B(y, z) * C(z, w)
-    // A(y, z) * B(z, x) * C(x, w)
-    mm(x, y) += cast<float>(cast<bfloat16_t>(cast<float>(A(r.x, y)) * cast<float>(B(r.y, r.x)))) * cast<float>(C(x, r.y));
 
-    int tile_x = 4;
-    int tile_y = 8;
-    int tile_r = 4;
-    Var rxi("rxi"), ryi("ryi");
-    RVar rxri("rxri"), rxro("rxro");
-    RVar ryri("ryri"), ryro("ryro");
+    if (target.has_feature(halide_target_feature_avx512_sapphirerapids)) {
+        mm(x, y) = cast<float>(0);
+        // mm(x, w) = A(x, y) * B(y, z) * C(z, w)
+        // A(y, z) * B(z, x) * C(x, w)
+        mm(x, y) += cast<float>(cast<bfloat16_t>(cast<float>(A(r.x, y)) * cast<float>(B(r.y, r.x)))) * cast<float>(C(x, r.y));
 
-    mm.compute_at(mm.in(), x)
-        .store_in(MemoryType::AMXTile)
-        .update()
-        .tile(x, y, rxi, ryi, tile_x, tile_y, TailStrategy::GuardWithIf)
-        .split(r.x, rxro, rxri, tile_r)
-        .split(r.y, ryro, ryri, tile_r)
-        .reorder({rxri, ryri, rxi, ryi, rxro, ryro, x, y})
-        .atomic()
-        .vectorize(rxri)
-        .vectorize(ryri)
-        .vectorize(rxi)
-        .vectorize(ryi);
+        int tile_x = 4;
+        int tile_y = 8;
+        int tile_r = 4;
+        Var rxi("rxi"), ryi("ryi");
+        RVar rxri("rxri"), rxro("rxro");
+        RVar ryri("ryri"), ryro("ryro");
 
-    Var ixi("ixi"), iyi("iyi");
-    mm.compute_at(mm.in(), x)
-        .tile(x, y, ixi, iyi, tile_x, tile_y)
-        .vectorize(ixi)
-        .vectorize(iyi);
+        mm.compute_at(mm.in(), x)
+            .store_in(MemoryType::AMXTile)
+            .update()
+            .tile(x, y, rxi, ryi, tile_x, tile_y, TailStrategy::GuardWithIf)
+            .split(r.x, rxro, rxri, tile_r)
+            .split(r.y, ryro, ryri, tile_r)
+            .reorder({rxri, ryri, rxi, ryi, rxro, ryro, x, y})
+            .atomic()
+            .vectorize(rxri)
+            .vectorize(ryri)
+            .vectorize(rxi)
+            .vectorize(ryi);
 
-    // schedule the consumer
-    Var mmxi("mmxi"), mmyi("mmyi");
-    mm.in()
-        .tile(x, y, mmxi, mmyi, tile_x, tile_y)
-        .vectorize(mmxi)
-        .vectorize(mmyi);
+        Var ixi("ixi"), iyi("iyi");
+        mm.compute_at(mm.in(), x)
+            .tile(x, y, ixi, iyi, tile_x, tile_y)
+            .vectorize(ixi)
+            .vectorize(iyi);
 
+        // schedule the consumer
+        Var mmxi("mmxi"), mmyi("mmyi");
+        mm.in()
+            .tile(x, y, mmxi, mmyi, tile_x, tile_y)
+            .vectorize(mmxi)
+            .vectorize(mmyi);
+
+    } else if (target.get_cuda_capability_lower_bound() >= 75) {
+
+        mm(x, y) = cast<float16_t>(0);
+        // mm(x, w) = A(x, y) * B(y, z) * C(z, w)
+        // A(y, z) * B(z, x) * C(x, w)
+        mm(x, y) += (A(r.x, y) * B(r.y, r.x)) * C(x, r.y);
+
+        int tile_x = 16;
+        int tile_y = 16;
+        int tile_rx = 16;
+        int tile_ry = 16;
+        Var xi("xi"), yi("yi");
+        Var rxi("rxi"), ryi("ryi");
+        RVar rxri("rxri"), rxro("rxro");
+        RVar ryri("ryri"), ryro("ryro");
+
+        mm.compute_at(mm.in(), x)
+            .store_in(MemoryType::WMMAAccumulator)
+            .update()
+            .split(x, x, rxi, tile_x)
+            .split(y, y, ryi, tile_y)
+            .split(r.x, rxro, rxri, tile_rx)
+            .split(r.y, ryro, ryri, tile_ry)
+            .reorder({rxri, ryri, rxi, ryi, x, y, rxro, ryro})
+            // .unroll(rxro)
+            // .unroll(ryro)
+            // .unroll(x)
+            // .unroll(y)
+            .atomic()
+            .vectorize(rxri)
+            .vectorize(ryri)
+            .vectorize(rxi)
+            .vectorize(ryi);
+
+        Var ixi("ixi"), iyi("iyi");
+        mm.split(x, x, rxi, tile_x)
+            .split(y, y, ryi, tile_y)
+            .vectorize(rxi)
+            .vectorize(ryi)
+            .unroll(y);
+
+        // schedule the consumer
+        Var mmxi("mmxi"), mmyi("mmyi");
+        mm.in()
+            .split(x, x, xi, tile_x)
+            .split(xi, xi, mmxi, tile_x)
+            .split(y, y, yi, tile_y)
+            .split(yi, yi, mmyi, tile_y)
+            .gpu_blocks(x, y)
+            .reorder({mmxi, mmyi, xi, yi, x, y})
+            .unroll(xi)
+            .unroll(yi)
+            .vectorize(mmxi)
+            .vectorize(mmyi);
+    }
     Func result = mm.in();
-
     result.compile_to_lowered_stmt("/tmp/matmul_flat_1x1.html", {A, B}, HTML, target);
 
     std::cout << "Success!\n";
@@ -68,7 +124,10 @@ bool matmul_bf16(Halide::Target target) {
 
 int main(int argc, char **argv) {
     freopen("/tmp/matmul_flat_1x1.log", "w", stderr);
-    Target target("x86-64-linux-avx512_sapphirerapids");
+    // Target target("x86-64-linux-avx512_sapphirerapids");
+    Target target = get_target_from_environment().with_feature(Target::CUDA).with_feature(Target::CUDACapability75)
+        // .with_feature(Target::Debug)
+        ;
 
     printf("Running AMX (bf16)\n");
     matmul_bf16(target);
