ae933b49999f2a3af5fe9c3ea3da55a76d1f6728
Yihong Zhang <yz489@cs.washington.edu>
2025-10-11 19:50:13 -0700

    finalize schedule code

--- apps/blur/halide_blur_generator.cpp
+++ apps/blur/halide_blur_generator.cpp
@@ -18,33 +18,43 @@ public:
         blur_x(x, y) = (input(x, y) + input(x + 1, y) + input(x + 2, y)) / 3;
         blur_y(x, y) = (blur_x(x, y) + blur_x(x, y + 1) + blur_x(x, y + 2)) / 3;
 
-        if (!get_target().has_gpu_feature() && !get_target().has_feature(Target::HVX)) {
-            // CPU schedule
-            // Compute blur_x as needed at each vector of the output.
-            // Halide will store blur_x in a circular buffer so its
-            // results can be re-used.
+        // inline everything 840 MP/s
 
-            // blur_x.compute_root();
-            // blur_y.compute_root();
+        // 1200 MP/s
+        // blur_x.compute_root();
 
-            const int VEC_LANES = 8;
-            blur_y
-                .compute_root()
-                .tile(x, y, xi, yi, 128, 32)
-                .vectorize(xi, VEC_LANES)
-                .parallel(y)
-                ;
-            blur_x
-                .compute_at(blur_y, x)
-                .vectorize(x, VEC_LANES);
+        // 2300 MP/s
+        // blur_x.compute_root();
+        // blur_x
+        //     .vectorize(x, 8);
+        // blur_y
+        //     .vectorize(x, 8);
 
-            // blur_y.print_loop_nest();
-            // exit(1);
-        } else if (get_target().has_gpu_feature()) {
-            generate_gpu();
-        } else if (get_target().has_feature(Target::HVX)) {
-        }
+        // 2800 MP/s
+        // blur_x.compute_root();
+        // blur_x
+        //     .split(y, y, yi, 32)
+        //     .parallel(y)
+        //     .vectorize(x, 8);
+        // blur_y
+        //     .split(y, y, yi, 32)
+        //     .parallel(y)
+        //     .vectorize(x, 8);
+
+        // blur_x.compute_at(blur_y, y);
+        blur_x
+            .vectorize(x, 8)
+            .unroll(x, 2);
+        blur_y
+            .split(y, y, yi, 8)
+            .parallel(y)
+            .vectorize(x, 8)
+            .unroll(x, 2);
+       
+        blur_y.print_loop_nest();
     }
+
+
 
     void generate_gpu() {
         int factor = sizeof(int) / sizeof(short);
