04c68a3c183b392c6720037e5a6a097007d9d254
Yihong Zhang <yz489@cs.washington.edu>
2025-10-12 00:03:50 -0700

    add an even better schedule

--- apps/blur/halide_blur_generator.cpp
+++ apps/blur/halide_blur_generator.cpp
@@ -18,38 +18,53 @@ public:
         blur_x(x, y) = (input(x, y) + input(x + 1, y) + input(x + 2, y)) / 3;
         blur_y(x, y) = (blur_x(x, y) + blur_x(x, y + 1) + blur_x(x, y + 2)) / 3;
 
-        // inline everything 840 MP/s
+        // inline everything 900 MP/s
 
         // 1200 MP/s
         // blur_x.compute_root();
 
-        // 2300 MP/s
+        // 2600 MP/s
         // blur_x.compute_root();
         // blur_x
-        //     .vectorize(x, 8);
+        //     .vectorize(x, 16);
         // blur_y
-        //     .vectorize(x, 8);
+        //     .vectorize(x, 16);
 
-        // 2800 MP/s
+        // 5000 MP/s
         // blur_x.compute_root();
         // blur_x
-        //     .split(y, y, yi, 32)
+        //     .split(y, y, yi, 1024)
         //     .parallel(y)
-        //     .vectorize(x, 8);
+        //     .vectorize(x, 16);
         // blur_y
-        //     .split(y, y, yi, 32)
+        //     .split(y, y, yi, 1024)
         //     .parallel(y)
-        //     .vectorize(x, 8);
+        //     .vectorize(x, 16);
 
+        // 12000 MP/s
         // blur_x.compute_at(blur_y, y);
-        blur_x
-            .vectorize(x, 8)
-            .unroll(x, 2);
-        blur_y
-            .split(y, y, yi, 8)
-            .parallel(y)
-            .vectorize(x, 8)
-            .unroll(x, 2);
+        // blur_x
+        //     .vectorize(x, 16)
+        //     .unroll(x, 2);
+        // blur_y
+        //     .split(y, y, yi, 8)
+        //     .parallel(y)
+        //     .vectorize(x, 16)
+        //     .unroll(x, 2);
+
+        // 14000 MP/s
+        // Var yii("yii");
+        // blur_x.compute_at(blur_y, x);
+        // blur_x
+        //     .vectorize(x, 16)
+        //     ;
+        // blur_y
+        //     .tile(x, y, xi, yi, 256, 512)
+        //     .split(yi, yi, yii, 16)
+        //     .reorder(xi, yii, x, yi, y)
+        //     .parallel(y)
+        //     .vectorize(xi, 16)
+        //     ;
        
         blur_y.print_loop_nest();
     }
